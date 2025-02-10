4113b2213a2daf073260556a6fa61eadd9bff25d
dheerajraghunathan <dheeraj183631001@iitgoa.ac.in>
2025-02-10 15:45:21 +0530

    Kokkos only version

--- src/topHat.cpp
+++ src/topHat.cpp
@@ -2,94 +2,109 @@
 
 #include "topHat.hpp"
 
-int main() {
-    
-    // **************************************************************
-	// Declare basic variables
-	// **************************************************************
-    int Nx = 8, Ny = 8;  // Global array size
-    // ####################### Done #################################
+using namespace std;
+int main(int argc, char* argv[]) {
+    Kokkos::initialize(argc, argv);
+    {
+        // **************************************************************
+        // Declare basic variables
+        // **************************************************************
+        int Nx = 8, Ny = 8;  // Global array size
+        // ####################### Done #################################
 
-    // **************************************************************
-	//  Declare Arrays
-	// **************************************************************
-    vector<vector<double>> globalArray(Nx, vector<double>(Ny));
-    // ####################### Done #################################
+        // **************************************************************
+        //  Declare Arrays
+        // **************************************************************
+        // Allocate memory for solution variables
+		Kokkos::View<double**, Kokkos::LayoutRight> globalArray("globalArray", Nx, Ny); //Layout right for MPI data transfer
+        Kokkos::View<double**, Kokkos::LayoutRight> globalArray1("globalArray1", Nx, Ny); //Layout right for MPI data transfer
+        Kokkos::View<double**, Kokkos::LayoutRight> globalArray2("globalArray2", Nx, Ny); //Layout right for MPI data transfer
+        // ####################### Done #################################
 
-    // **************************************************************
-	// Generate random values to the global array
-	// **************************************************************
-    generateRandomData(globalArray, Nx, Ny);
-    // ####################### Done #################################
-    
-    // **************************************************************
-	// Print the array
-	// **************************************************************
-    cout << "Generated random array:\n";
-    for (const auto &row : globalArray) {
-        for (double val : row)
-            cout << val << "\t";
-        cout << endl;
+        // **************************************************************
+        // Generate random values to the global array
+        // **************************************************************
+        generateRandomData(globalArray, Nx, Ny);
+        // ####################### Done #################################
+        
+        // **************************************************************
+        // Print the array
+        // **************************************************************
+        // Allocate an array in the host space & copy the globalArray
+        auto globalArray_output = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), globalArray);
+        cout << "Generated random array:\n";
+        for (int i = 0; i < Nx; i++) {
+            for (int j = 0; j < Ny; j++) {
+                cout << globalArray_output(i,j) << "\t";
+            }
+            cout << "\n";
+        }
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Declare extra arrays to test different BCs
+        // **************************************************************
+        Kokkos::deep_copy(globalArray1, globalArray);
+        Kokkos::deep_copy(globalArray2, globalArray);
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Apply filtering
+        // **************************************************************
+        applyTopHatFilter(globalArray, Nx, Ny);
+        Kokkos::deep_copy(globalArray_output, globalArray);  // Copy updated data from device to host
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Print the array
+        // **************************************************************
+        cout << "Filtered array - No BC:\n";
+        for (int i = 0; i < Nx; i++) {
+            for (int j = 0; j < Ny; j++) {
+                cout << globalArray_output(i,j) << "\t";
+            }
+            cout << "\n";
+        }
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Apply filtering - Periodic BC
+        // **************************************************************
+        applyTopHatFilterPeriodicBC(globalArray1, Nx, Ny);
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Print the array
+        // **************************************************************
+        Kokkos::deep_copy(globalArray_output, globalArray1);  // Copy updated data from device to host
+        cout << "Filtered array - Periodic BC:\n";
+        for (int i = 0; i < Nx; i++) {
+            for (int j = 0; j < Ny; j++) {
+                cout << globalArray_output(i,j) << "\t";
+            }
+            cout << "\n";
+        }
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Apply filtering - Zero BC
+        // **************************************************************
+        applyTopHatFilterZeroBC(globalArray2, Nx, Ny);
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Print the array
+        // **************************************************************
+        Kokkos::deep_copy(globalArray_output, globalArray2);  // Copy updated data from device to host
+        cout << "Filtered array - Zero BC:\n";
+        for (int i = 0; i < Nx; i++) {
+            for (int j = 0; j < Ny; j++) {
+                std::cout << globalArray_output(i,j) << "\t";
+            }
+            std::cout << "\n";
+        }
+        // ####################### Done #################################
     }
-    // ####################### Done #################################
-
-    // **************************************************************
-    // Declare extra arrays to test different BCs
-    // **************************************************************
-    vector<vector<double>> globalArray1 = globalArray; 
-    vector<vector<double>> globalArray2 = globalArray; 
-    // ####################### Done #################################
-
-    // **************************************************************
-	// Apply filtering
-	// **************************************************************
-    applyTopHatFilter(globalArray, Nx, Ny);
-    // ####################### Done #################################
-
-    // **************************************************************
-	// Print the array
-	// **************************************************************
-    cout << "Filtered array - No BC:\n";
-    for (const auto &row : globalArray) {
-        for (double val : row)
-            cout << val << "\t";
-        cout << endl;
-    }
-    // ####################### Done #################################
-
-    // **************************************************************
-	// Apply filtering - Periodic BC
-	// **************************************************************
-    applyTopHatFilterPeriodicBC(globalArray1, Nx, Ny);
-    // ####################### Done #################################
-
-    // **************************************************************
-	// Print the array
-	// **************************************************************
-    cout << "Filtered array - Periodic BC:\n";
-    for (const auto &row : globalArray1) {
-        for (double val : row)
-            cout << val << "\t";
-        cout << endl;
-    }
-    // ####################### Done #################################
-
-    // **************************************************************
-	// Apply filtering - Zero BC
-	// **************************************************************
-    applyTopHatFilterZeroBC(globalArray2, Nx, Ny);
-    // ####################### Done #################################
-
-    // **************************************************************
-	// Print the array
-	// **************************************************************
-    cout << "Filtered array - Zero BC:\n";
-    for (const auto &row : globalArray2) {
-        for (double val : row)
-            cout << val << "\t";
-        cout << endl;
-    }
-    // ####################### Done #################################
-
+    Kokkos::finalize();
     return 0;
 }
--- src/topHat.hpp
+++ src/topHat.hpp
@@ -3,21 +3,26 @@
 
 #include <vector>
 #include <cstdlib>
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Random.hpp>
 
 using namespace std;
 
 // ==============================================================
 // ====== Function to generate random values to a 2D array ======
 // ==============================================================
-void generateRandomData(vector<vector<double>> &array, const int &Nx, const int &Ny) {
-    srand(time(nullptr));   // Seed the random number generator
+void generateRandomData(Kokkos::View<double**, Kokkos::LayoutRight> array, const int &Nx, const int &Ny) {
+    // Create random number pool
+    Kokkos::Random_XorShift64_Pool<> rand_pool(/*seed=*/12345);
 
-    // Fill the array with random values (0 to 99)
-    for (int i = 0; i < Nx; i++) {
-        for (int j = 0; j < Ny; j++) {
-            array[i][j] = rand() % 100;  // Random numbers from 0 to 99
-        }
-    }
+    // Parallel random number generation
+    Kokkos::parallel_for("GenerateRandom2D",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
+        KOKKOS_LAMBDA(int i, int j) {
+            auto rand_gen = rand_pool.get_state();
+            array(i, j) = rand_gen.drand(0.0, 100.0);  // Random double in [min_val, max_val)
+            rand_pool.free_state(rand_gen);
+    });
 
 }
 // ####################### Done #################################
@@ -25,17 +30,18 @@ void generateRandomData(vector<vector<double>> &array, const int &Nx, const int 
 // ==============================================================
 // ======= Function to apply top-hat filter to a 2D array =======
 // ==============================================================
-void applyTopHatFilter(vector<vector<double>> &array, const int &Nx, const int &Ny) {
-    vector<vector<double>> tempArray = array;      // Copying to a temp array
+void applyTopHatFilter(Kokkos::View<double**, Kokkos::LayoutRight> array, const int &Nx, const int &Ny) {
+    Kokkos::View<double**, Kokkos::LayoutRight> tempArray("tempArray", Nx, Ny);
+    Kokkos::deep_copy(tempArray, array);          // Copying to a temp array
     const double inv9 = 1.0/9.0;
-    for (int i = 1; i < Nx-1; i++) {
-        for (int j = 1; j < Ny-1; j++) {
-            array[i][j]  =  inv9 * (tempArray[i][j] + tempArray[i+1][j]   + tempArray[i-1][j]   +
-                                                      tempArray[i][j+1]   + tempArray[i][j-1]   +
-                                                      tempArray[i-1][j-1] + tempArray[i-1][j+1] +
-                                                      tempArray[i+1][j-1] + tempArray[i+1][j+1] ) ;
-        }
-    }
+    Kokkos::parallel_for("TopHatFilter",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({1,1}, {Nx-1, Ny-1}),
+        KOKKOS_LAMBDA(int i, int j) {
+            array(i,j)  =  inv9 * (tempArray(i,j) + tempArray(i+1,j)   + tempArray(i-1,j)   +
+                                                      tempArray(i,j+1)   + tempArray(i,j-1)   +
+                                                      tempArray(i-1,j-1) + tempArray(i-1,j+1) +
+                                                      tempArray(i+1,j-1) + tempArray(i+1,j+1) ) ;
+    });
 
 }
 // ####################### Done #################################
@@ -43,35 +49,36 @@ void applyTopHatFilter(vector<vector<double>> &array, const int &Nx, const int &
 // ==============================================================
 // ===== Function to apply top-hat filter with Periodic BC ======
 // ==============================================================
-void applyTopHatFilterPeriodicBC(vector<vector<double>> &array, const int &Nx, const int &Ny) {
-    vector<vector<double>> tempArray = array;      // Copying to a temp array
+void applyTopHatFilterPeriodicBC(Kokkos::View<double**, Kokkos::LayoutRight> array, const int &Nx, const int &Ny) {
+    Kokkos::View<double**, Kokkos::LayoutRight> tempArray("tempArray", Nx, Ny);
+    Kokkos::deep_copy(tempArray, array);          // Copying to a temp array
     const double inv9 = 1.0/9.0;
-    for (int i = 0; i < Nx; i++) {
-        for (int j = 0; j < Ny; j++) {
-            double i_east  = i+1;
-            double i_west  = i-1;
-            double j_north = j+1;
-            double j_south = j-1;
+    Kokkos::parallel_for("TopHatFilter",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
+        KOKKOS_LAMBDA(int i, int j) {
+            int i_east  = i+1;
+            int i_west  = i-1;
+            int j_north = j+1;
+            int j_south = j-1;
 
             if (i==0)    i_west  = Nx-1;
             if (i==Nx-1) i_east  = 0;
             if (j==0)    j_south = Ny-1;
             if (j==Ny-1) j_north = 0;
  
-            double centre    = tempArray[i][j];
-            double east      = tempArray[i_east][j];
-            double west      = tempArray[i_west][j];
-            double north     = tempArray[i][j_north];
-            double south     = tempArray[i][j_south];
-            double northeast = tempArray[i_east][j_north];
-            double northwest = tempArray[i_west][j_north];
-            double southeast = tempArray[i_east][j_south];
-            double southwest = tempArray[i_west][j_south];
+            double centre    = tempArray(i,j);
+            double east      = tempArray(i_east,j);
+            double west      = tempArray(i_west,j);
+            double north     = tempArray(i,j_north);
+            double south     = tempArray(i,j_south);
+            double northeast = tempArray(i_east,j_north);
+            double northwest = tempArray(i_west,j_north);
+            double southeast = tempArray(i_east,j_south);
+            double southwest = tempArray(i_west,j_south);
 
-            array[i][j]  =  inv9 * ( centre + east + west + north + south +
+            array(i,j)  =  inv9 * ( centre + east + west + north + south +
                                      northeast + northwest + southeast + southwest ) ;
-        }
-    }
+    });
 
 }
 // ####################### Done #################################
@@ -79,39 +86,40 @@ void applyTopHatFilterPeriodicBC(vector<vector<double>> &array, const int &Nx, c
 // ==============================================================
 // ======= Function to apply top-hat filter with Zero BC  =======
 // ==============================================================
-void applyTopHatFilterZeroBC(vector<vector<double>> &array, const int &Nx, const int &Ny) {
-    vector<vector<double>> tempArray(Nx+2, vector<double>(Ny+2, 0)); // Temp array initialised to zero
-    for (int i = 0; i < Nx; ++i) {
-        for (int j = 0; j < Ny; ++j) {
-            tempArray[i+1][j+1] = array[i][j]; // Offset by 1 to create zero borders
-        }
-    }
+void applyTopHatFilterZeroBC(Kokkos::View<double**, Kokkos::LayoutRight> array, const int &Nx, const int &Ny) {
+    Kokkos::View<double**, Kokkos::LayoutRight> tempArray("tempArray", Nx+2, Ny+2);
+    Kokkos::deep_copy(tempArray, 0.0); // Temp array initialised to zero
+    Kokkos::parallel_for("TopHatFilter",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
+        KOKKOS_LAMBDA(int i, int j) {
+            tempArray(i+1,j+1) = array(i,j); // Offset by 1 to create zero borders
+    });
     const double inv9 = 1.0/9.0;
-    for (int i = 0; i < Nx; i++) {
-        for (int j = 0; j < Ny; j++) {
+    Kokkos::parallel_for("TopHatFilter",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
+        KOKKOS_LAMBDA(int i, int j) {
             // Indices of tempArray
             int ii = i+1;  
             int jj = j+1;
 
-            double i_east  = ii+1;
-            double i_west  = ii-1;
-            double j_north = jj+1;
-            double j_south = jj-1;
+            int i_east  = ii+1;
+            int i_west  = ii-1;
+            int j_north = jj+1;
+            int j_south = jj-1;
             
-            double centre    = tempArray[ii][jj];
-            double east      = tempArray[i_east][jj];
-            double west      = tempArray[i_west][jj];
-            double north     = tempArray[ii][j_north];
-            double south     = tempArray[ii][j_south];
-            double northeast = tempArray[i_east][j_north];
-            double northwest = tempArray[i_west][j_north];
-            double southeast = tempArray[i_east][j_south];
-            double southwest = tempArray[i_west][j_south];
+            double centre    = tempArray(ii,jj);
+            double east      = tempArray(i_east,jj);
+            double west      = tempArray(i_west,jj);
+            double north     = tempArray(ii,j_north);
+            double south     = tempArray(ii,j_south);
+            double northeast = tempArray(i_east,j_north);
+            double northwest = tempArray(i_west,j_north);
+            double southeast = tempArray(i_east,j_south);
+            double southwest = tempArray(i_west,j_south);
 
-            array[i][j]  =  inv9 * ( centre + east + west + north + south +
+            array(i,j)  =  inv9 * ( centre + east + west + north + south +
                                      northeast + northwest + southeast + southwest ) ;
-        }
-    }
+    });
 
 }
 // ####################### Done #################################

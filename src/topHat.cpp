1fd4cea2089fbec9d61ccbd20c8c1312b867f544
dheerajraghunathan <dheeraj183631001@iitgoa.ac.in>
2025-02-12 14:39:21 +0530

    MPI initial implementation

--- src/topHat.cpp
+++ src/topHat.cpp
@@ -4,27 +4,74 @@
 
 using namespace std;
 int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
     Kokkos::initialize(argc, argv);
     {
         // **************************************************************
         // Declare basic variables
         // **************************************************************
-        int Nx = 8, Ny = 8;  // Global array size
-        // ####################### Done #################################
+        int Ngx = 8, Ngy = 8;   // Global array size
+        int Npx = 2, Npy = 2;   // No. of processors in each direction
+        int Nx  = Ngx/Npx;      // Local array size in X direction
+        int Ny  = Ngy/Npy;      // Local array size in Y direction
+        int numGhost = 2;
+		Nx = Nx + numGhost;
+		Ny = Ny + numGhost;
+		// ####################### Done #################################
+
+        // **************************************************************
+	    // Set MPI Parameters
+	    // **************************************************************
+		// Number of processors
+		int procsize; 					
+		MPI_Comm_size(MPI_COMM_WORLD, &procsize);
+
+		// Ask MPI to decompose processors in a Cartesian grid
+		int dims[2] = {Npx, Npy};
+		MPI_Dims_create(procsize, 2, dims);
+
+		// If periodic BC is true then it connects appropriate processors
+		int periods[2];
+		periods[0] = true;
+		periods[1] = true;
+
+		// Let MPI assign arbitrary ranks if it seems necessary
+		int reorder = false;
+
+		// Define the communicator
+		MPI_Comm comm;
+		MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &comm);
+
+		// My rank in the new communicator
+		int myRank;
+		MPI_Comm_rank(comm, &myRank);
+
+		// Get my coordinates in the new communicator
+		int myCoords[2];
+		MPI_Cart_coords(comm, myRank, 2, myCoords);
+
+        // Get global index ranges for each processor
+		int ibegin = myCoords[0]*(Nx-numGhost);
+		int jbegin = myCoords[1]*(Ny-numGhost);
+	    // ####################### Done #################################
 
         // **************************************************************
         //  Declare Arrays
         // **************************************************************
-        // Allocate memory for solution variables
-		Kokkos::View<double**, Kokkos::LayoutRight> globalArray("globalArray", Nx, Ny); //Layout right for MPI data transfer
-        Kokkos::View<double**, Kokkos::LayoutRight> globalArray1("globalArray1", Nx, Ny); //Layout right for MPI data transfer
-        Kokkos::View<double**, Kokkos::LayoutRight> globalArray2("globalArray2", Nx, Ny); //Layout right for MPI data transfer
+        Kokkos::View<double**, Kokkos::LayoutRight> globalArray("globalArray", Ngx, Ngy);
+		Kokkos::View<double**, Kokkos::LayoutRight> localArray("localArray", Nx, Ny);
         // ####################### Done #################################
 
         // **************************************************************
         // Generate random values to the global array
         // **************************************************************
-        generateRandomData(globalArray, Nx, Ny);
+        if (myRank==0) generateRandomData(globalArray, Ngx, Ngy);
+        // ####################### Done #################################
+
+        // **************************************************************
+        // Broadcast the global matrix with random values from rank 0 to all other ranks
+        // **************************************************************
+        MPI_Bcast(globalArray.data(), Ngx * Ngy, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         // ####################### Done #################################
         
         // **************************************************************
@@ -32,79 +79,87 @@ int main(int argc, char* argv[]) {
         // **************************************************************
         // Allocate an array in the host space & copy the globalArray
         auto globalArray_output = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), globalArray);
-        cout << "Generated random array:\n";
-        for (int i = 0; i < Nx; i++) {
-            for (int j = 0; j < Ny; j++) {
-                cout << globalArray_output(i,j) << "\t";
+        if (myRank==0) {
+            cout << "Generated random array:\n";
+            for (int i = 0; i < Ngx; i++) {
+                for (int j = 0; j < Ngy; j++) {
+                    cout << globalArray_output(i,j) << "\t";
+                }
+                cout << "\n";
             }
-            cout << "\n";
         }
         // ####################### Done #################################
 
         // **************************************************************
-        // Declare extra arrays to test different BCs
+        // Divide the global array to subarrays
         // **************************************************************
-        Kokkos::deep_copy(globalArray1, globalArray);
-        Kokkos::deep_copy(globalArray2, globalArray);
+        Kokkos::parallel_for("copy global values to local arrays",
+            Kokkos::MDRangePolicy<Kokkos::Rank<2>>({1,1}, {Nx-1, Ny-1}),
+            KOKKOS_LAMBDA(int i, int j) {
+                int ii = ibegin + i - 1;    // -1 because global array has no Ghost nodes
+                int jj = jbegin + j - 1;    // -1 because global array has no Ghost nodes
+                localArray(i,j)  =  globalArray(ii,jj);
+        });
+        // ####################### Done #################################
+        
+        // **************************************************************
+        // Halo data transfer - Boundary nodes treated as periodic
+        // **************************************************************
+        haloTransfer(localArray, Nx, Ny, comm);
         // ####################### Done #################################
 
         // **************************************************************
-        // Apply filtering
+        // Apply filtering - Periodic Boundaries
         // **************************************************************
-        applyTopHatFilter(globalArray, Nx, Ny);
-        Kokkos::deep_copy(globalArray_output, globalArray);  // Copy updated data from device to host
+        applyTopHatFilter(localArray, Nx, Ny);
         // ####################### Done #################################
-
+        
         // **************************************************************
-        // Print the array
+        // Print local arrays
         // **************************************************************
-        cout << "Filtered array - No BC:\n";
-        for (int i = 0; i < Nx; i++) {
-            for (int j = 0; j < Ny; j++) {
-                cout << globalArray_output(i,j) << "\t";
+        auto localArray_output = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), localArray);
+        Kokkos::deep_copy(localArray_output, localArray);  // Copy updated data from device to host
+        Kokkos::fence();
+        if (myRank==0) {
+            cout << "Local Array - Rank 0:\n";
+            for (int i = 0; i < Nx; i++) {
+                for (int j = 0; j < Ny; j++) {
+                    cout << localArray_output(i,j) << "\t";
+                }
+                cout << "\n";
             }
-            cout << "\n";
+        }
+        if (myRank==1) {
+            cout << "Local Array - Rank 1:\n";
+            for (int i = 0; i < Nx; i++) {
+                for (int j = 0; j < Ny; j++) {
+                    cout << localArray_output(i,j) << "\t";
+                }
+                cout << "\n";
+            }
+        }
+        if (myRank==2) {
+            cout << "Local Array - Rank 2:\n";
+            for (int i = 0; i < Nx; i++) {
+                for (int j = 0; j < Ny; j++) {
+                    cout << localArray_output(i,j) << "\t";
+                }
+                cout << "\n";
+            }
+        }
+        if (myRank==3) {
+            cout << "Local Array - Rank 3:\n";
+            for (int i = 0; i < Nx; i++) {
+                for (int j = 0; j < Ny; j++) {
+                    cout << localArray_output(i,j) << "\t";
+                }
+                cout << "\n";
+            }
         }
         // ####################### Done #################################
 
-        // **************************************************************
-        // Apply filtering - Periodic BC
-        // **************************************************************
-        applyTopHatFilterPeriodicBC(globalArray1, Nx, Ny);
-        // ####################### Done #################################
-
-        // **************************************************************
-        // Print the array
-        // **************************************************************
-        Kokkos::deep_copy(globalArray_output, globalArray1);  // Copy updated data from device to host
-        cout << "Filtered array - Periodic BC:\n";
-        for (int i = 0; i < Nx; i++) {
-            for (int j = 0; j < Ny; j++) {
-                cout << globalArray_output(i,j) << "\t";
-            }
-            cout << "\n";
-        }
-        // ####################### Done #################################
-
-        // **************************************************************
-        // Apply filtering - Zero BC
-        // **************************************************************
-        applyTopHatFilterZeroBC(globalArray2, Nx, Ny);
-        // ####################### Done #################################
-
-        // **************************************************************
-        // Print the array
-        // **************************************************************
-        Kokkos::deep_copy(globalArray_output, globalArray2);  // Copy updated data from device to host
-        cout << "Filtered array - Zero BC:\n";
-        for (int i = 0; i < Nx; i++) {
-            for (int j = 0; j < Ny; j++) {
-                std::cout << globalArray_output(i,j) << "\t";
-            }
-            std::cout << "\n";
-        }
-        // ####################### Done #################################
     }
     Kokkos::finalize();
+    MPI_Finalize();
     return 0;
 }
--- src/topHat.hpp
+++ src/topHat.hpp
@@ -6,6 +6,8 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Random.hpp>
 
+#include <mpi.h>
+
 using namespace std;
 
 // ==============================================================
@@ -14,7 +16,7 @@ using namespace std;
 void generateRandomData(Kokkos::View<double**, Kokkos::LayoutRight> array, const int &Nx, const int &Ny) {
     // Create random number pool
     Kokkos::Random_XorShift64_Pool<> rand_pool(/*seed=*/12345);
-
+    
     // Parallel random number generation
     Kokkos::parallel_for("GenerateRandom2D",
         Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
@@ -23,7 +25,6 @@ void generateRandomData(Kokkos::View<double**, Kokkos::LayoutRight> array, const
             array(i, j) = rand_gen.drand(0.0, 100.0);  // Random double in [min_val, max_val)
             rand_pool.free_state(rand_gen);
     });
-
 }
 // ####################### Done #################################
 
@@ -34,13 +35,14 @@ void applyTopHatFilter(Kokkos::View<double**, Kokkos::LayoutRight> array, const 
     Kokkos::View<double**, Kokkos::LayoutRight> tempArray("tempArray", Nx, Ny);
     Kokkos::deep_copy(tempArray, array);          // Copying to a temp array
     const double inv9 = 1.0/9.0;
+
     Kokkos::parallel_for("TopHatFilter",
         Kokkos::MDRangePolicy<Kokkos::Rank<2>>({1,1}, {Nx-1, Ny-1}),
         KOKKOS_LAMBDA(int i, int j) {
             array(i,j)  =  inv9 * (tempArray(i,j) + tempArray(i+1,j)   + tempArray(i-1,j)   +
-                                                      tempArray(i,j+1)   + tempArray(i,j-1)   +
-                                                      tempArray(i-1,j-1) + tempArray(i-1,j+1) +
-                                                      tempArray(i+1,j-1) + tempArray(i+1,j+1) ) ;
+                                                    tempArray(i,j+1)   + tempArray(i,j-1)   +
+                                                    tempArray(i-1,j-1) + tempArray(i-1,j+1) +
+                                                    tempArray(i+1,j-1) + tempArray(i+1,j+1) ) ;
     });
 
 }
@@ -49,78 +51,107 @@ void applyTopHatFilter(Kokkos::View<double**, Kokkos::LayoutRight> array, const 
 // ==============================================================
 // ===== Function to apply top-hat filter with Periodic BC ======
 // ==============================================================
-void applyTopHatFilterPeriodicBC(Kokkos::View<double**, Kokkos::LayoutRight> array, const int &Nx, const int &Ny) {
-    Kokkos::View<double**, Kokkos::LayoutRight> tempArray("tempArray", Nx, Ny);
-    Kokkos::deep_copy(tempArray, array);          // Copying to a temp array
-    const double inv9 = 1.0/9.0;
-    Kokkos::parallel_for("TopHatFilter",
-        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
-        KOKKOS_LAMBDA(int i, int j) {
-            int i_east  = i+1;
-            int i_west  = i-1;
-            int j_north = j+1;
-            int j_south = j-1;
+void haloTransfer( Kokkos::View<double**, Kokkos::LayoutRight> localArray, 
+                   const int &Nx, const int &Ny, const MPI_Comm& comm ) {
+    int myRank;
+	MPI_Comm_rank(comm, &myRank);
 
-            if (i==0)    i_west  = Nx-1;
-            if (i==Nx-1) i_east  = 0;
-            if (j==0)    j_south = Ny-1;
-            if (j==Ny-1) j_north = 0;
- 
-            double centre    = tempArray(i,j);
-            double east      = tempArray(i_east,j);
-            double west      = tempArray(i_west,j);
-            double north     = tempArray(i,j_north);
-            double south     = tempArray(i,j_south);
-            double northeast = tempArray(i_east,j_north);
-            double northwest = tempArray(i_west,j_north);
-            double southeast = tempArray(i_east,j_south);
-            double southwest = tempArray(i_west,j_south);
-
-            array(i,j)  =  inv9 * ( centre + east + west + north + south +
-                                     northeast + northwest + southeast + southwest ) ;
-    });
-
-}
-// ####################### Done #################################
-
-// ==============================================================
-// ======= Function to apply top-hat filter with Zero BC  =======
-// ==============================================================
-void applyTopHatFilterZeroBC(Kokkos::View<double**, Kokkos::LayoutRight> array, const int &Nx, const int &Ny) {
-    Kokkos::View<double**, Kokkos::LayoutRight> tempArray("tempArray", Nx+2, Ny+2);
-    Kokkos::deep_copy(tempArray, 0.0); // Temp array initialised to zero
-    Kokkos::parallel_for("TopHatFilter",
-        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
-        KOKKOS_LAMBDA(int i, int j) {
-            tempArray(i+1,j+1) = array(i,j); // Offset by 1 to create zero borders
-    });
-    const double inv9 = 1.0/9.0;
-    Kokkos::parallel_for("TopHatFilter",
-        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0,0}, {Nx, Ny}),
-        KOKKOS_LAMBDA(int i, int j) {
-            // Indices of tempArray
-            int ii = i+1;  
-            int jj = j+1;
-
-            int i_east  = ii+1;
-            int i_west  = ii-1;
-            int j_north = jj+1;
-            int j_south = jj-1;
+    // Find neighbours in the new communicator
+    int neigborProc[4];
+    enum DIRECTIONS {LEFT, RIGHT, DOWN, UP};
+    // Let consider dims[0] = X, so the shift tells us our left and right neighbours
+    MPI_Cart_shift(comm, 0, 1, &neigborProc[LEFT], &neigborProc[RIGHT]);
+    // Let consider dims[1] = Y, so the shift tells us our up and down neighbours
+    MPI_Cart_shift(comm, 1, 1, &neigborProc[DOWN], &neigborProc[UP]);
             
-            double centre    = tempArray(ii,jj);
-            double east      = tempArray(i_east,jj);
-            double west      = tempArray(i_west,jj);
-            double north     = tempArray(ii,j_north);
-            double south     = tempArray(ii,j_south);
-            double northeast = tempArray(i_east,j_north);
-            double northwest = tempArray(i_west,j_north);
-            double southeast = tempArray(i_east,j_south);
-            double southwest = tempArray(i_west,j_south);
+    MPI_Status status;
+    MPI_Request request[8] = {MPI_REQUEST_NULL};
 
-            array(i,j)  =  inv9 * ( centre + east + west + north + south +
-                                     northeast + northwest + southeast + southwest ) ;
-    });
+    // ==========  Data tranfer ======
+    //i = 1  	 ---->>>>  	i = Nx-1
+    //i = Nx-2   ---->>>>  	i = 0
+    //j = 1  	 ---->>>>  	j = Ny-1
+    //j = Ny-2   ---->>>>  	j = 0
 
+    // STEP1 : Sending information to the bottom processor and receiving from top
+    auto sendBuffer1 = Kokkos::subview(localArray, 1, Kokkos::ALL() );
+    auto recvBuffer1 = Kokkos::subview(localArray, Nx-1, Kokkos::ALL() );
+    if (neigborProc[0] != MPI_PROC_NULL)
+    {
+        Kokkos::fence();  // Ensure data readiness
+        MPI_Isend(sendBuffer1.data(), Ny, MPI_DOUBLE, neigborProc[0], myRank,   comm, &request[0]);
+    }
+    if (neigborProc[1] != MPI_PROC_NULL)
+    {
+        MPI_Irecv(recvBuffer1.data(), Ny, MPI_DOUBLE, neigborProc[1], neigborProc[1],   comm,  &request[1]);
+    }
+    MPI_Wait(&request[0], &status); //blocks and waits for destination process to receive data
+    MPI_Wait(&request[1], &status); //blocks and waits for destination process to receive data
+    
+    // STEP2 : Sending information to the top processor and receiving from bottom
+    auto sendBuffer2 = Kokkos::subview(localArray, Nx-2, Kokkos::ALL() );
+    auto recvBuffer2 = Kokkos::subview(localArray, 0, Kokkos::ALL() );
+    if (neigborProc[1] != MPI_PROC_NULL)
+    {
+        Kokkos::fence();  // Ensure data readiness
+        MPI_Isend(sendBuffer2.data(), Ny, MPI_DOUBLE, neigborProc[1], myRank,   comm, &request[2]);
+    }
+    if (neigborProc[0] != MPI_PROC_NULL)
+    {
+        MPI_Irecv(recvBuffer2.data(), Ny, MPI_DOUBLE, neigborProc[0], neigborProc[0],   comm,  &request[3]);
+    }
+    MPI_Wait(&request[2], &status); //blocks and waits for destination process to receive data
+    MPI_Wait(&request[3], &status); //blocks and waits for destination process to receive data
+
+    // =========================================================================================
+    /* For STEP1 and STEP2, subviews yield contiguous memory while using Kokkos::LayoutRight. 
+       For STEP3 and STEP4, subviews yield non-contiguous memory & hence send/recv require
+       packing the data into a contiguous buffer before MPI data transfer. */
+    // =========================================================================================
+    
+    // STEP3 : Sending information to the left processor and receiving from right
+    Kokkos::View<double*> sendBuffer3("sendBuffer", Nx);
+    Kokkos::View<double*> recvBuffer3("recvBuffer", Nx);
+    if (neigborProc[2] != MPI_PROC_NULL)
+    {
+        Kokkos::parallel_for(Nx, KOKKOS_LAMBDA(int i) {
+            sendBuffer3(i) = localArray(i,1);
+        });
+        Kokkos::fence();
+        MPI_Isend(sendBuffer3.data(), Nx, MPI_DOUBLE, neigborProc[2], myRank, comm, &request[4]);
+    }
+    if (neigborProc[3] != MPI_PROC_NULL)
+    {
+        MPI_Irecv(recvBuffer3.data(), Nx, MPI_DOUBLE, neigborProc[3], neigborProc[3], comm, &request[5]);
+        MPI_Wait(&request[5], &status); //blocks and waits for destination process to receive data
+        Kokkos::parallel_for(Nx, KOKKOS_LAMBDA(int i) {
+            localArray(i,Ny-1) = recvBuffer3(i);
+        });
+        Kokkos::fence();
+    }
+    MPI_Wait(&request[4], &status); //blocks and waits for destination process to receive data
+    
+    // STEP4 : Sending information to the right processor and receiving from left
+    Kokkos::View<double*> sendBuffer4("sendBuffer", Nx);
+    Kokkos::View<double*> recvBuffer4("recvBuffer", Nx);
+    if (neigborProc[3] != MPI_PROC_NULL)
+    {
+        Kokkos::parallel_for(Nx, KOKKOS_LAMBDA(int i) {
+            sendBuffer4(i) = localArray(i,Ny-2);
+        });
+        Kokkos::fence();
+        MPI_Isend(sendBuffer4.data(), Nx, MPI_DOUBLE, neigborProc[3], myRank, comm, &request[6]);
+    }
+    if (neigborProc[2] != MPI_PROC_NULL)
+    {
+        MPI_Irecv(recvBuffer4.data(), Nx, MPI_DOUBLE, neigborProc[2], neigborProc[2],   comm,  &request[7]);
+        MPI_Wait(&request[7], &status); //blocks and waits for destination process to receive data
+        Kokkos::parallel_for(Nx, KOKKOS_LAMBDA(int i) {
+            localArray(i,0) = recvBuffer4(i);
+        });
+        Kokkos::fence();
+    }
+    MPI_Wait(&request[6], &status); //blocks and waits for destination process to receive data   
 }
 // ####################### Done #################################
 
